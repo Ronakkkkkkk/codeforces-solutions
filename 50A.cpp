9f9374ef3270eaa8fd313a36270d2ed5c4b8accd
Ronakkkkkkk <ronaks2022@gmail.com>
2026-01-31 00:17:43 +0530

    Add files via upload

---
+++ 50A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int M, N;
+    cin>>M>>N;
+       
+    cout<<(M*N)/2<<endl;
+    return 0;
+}
